0bd641e4eff9a9f9bff0f984d8c0a5f371e74b65
tu-nguyen <valcrune@gmail.com>
2021-02-27 21:03:25 -0500

    fixed carousel on create

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,33 +1,30 @@
-// carousel from homepage
-var homeCarousel = document.querySelector('#homeCarousel')
-var carousel = new bootstrap.Carousel(homeCarousel, {
-//   interval: 7000,
-  wrap: true
-})
 
-// templates carousel
-var templateCarousel = document.querySelector('#templateCarousel')
-var carousel2 = new bootstrap.Carousel(templateCarousel, {
-  interval: false,
-  keyboard: true,
-  ride:false,
-  wrap: true
-})
+  // carousel from homepage
+  var homeCarousel = document.querySelector('#homeCarousel')
+  var carousel = new bootstrap.Carousel(homeCarousel, {
+  //   interval: 7000,
+    wrap: true
+  })
 
+  // templates carousel
+  var templateCarousel = document.querySelector('#templateCarousel')
+  var carousel2 = new bootstrap.Carousel(templateCarousel, {
+    interval: false,
+    keyboard: true,
+    ride:false,
+    wrap: true
+  })
 
-$(document).ready(function(){
-
-  var selectedTemplate = $('#templateCarousel .carousel-inner .carousel-item card');
+  var selectedTemplate = $('.card');
   $selectedTemplate.click(function(e){
-    $('#templateCarousel .carousel-inner .carousel-item card').removeClass('selectedTemplate');
-          e.target.classList.add('selectedTemplate');
+    $('#templateCarousel .carousel-inner .carousel-item .row .card').removeClass('selectedTemplate');
+    e.target.classList.add('selectedTemplate');
   })
 
 
 
 
 
-});
 
 
 
