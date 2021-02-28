eef193e9d7faf08e1e815f5063632ea0c4faf9ba
tu-nguyen <valcrune@gmail.com>
2021-02-27 19:44:45 -0500

    added carousel to template section

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,5 +1,34 @@
-var myCarousel = document.querySelector('#myCarousel')
-var carousel = new bootstrap.Carousel(myCarousel, {
+// carousel from homepage
+var homeCarousel = document.querySelector('#homeCarousel')
+var carousel = new bootstrap.Carousel(homeCarousel, {
 //   interval: 7000,
   wrap: true
 })
+
+// templates carousel
+var templateCarousel = document.querySelector('#templateCarousel')
+var carousel2 = new bootstrap.Carousel(templateCarousel, {
+  interval: false,
+  keyboard: true,
+  ride:false,
+  wrap: true
+})
+
+
+$(document).ready(function(){
+
+  var selectedTemplate = $('#templateCarousel .carousel-inner .carousel-item card');
+  $selectedTemplate.click(function(e){
+    $('#templateCarousel .carousel-inner .carousel-item card').removeClass('selectedTemplate');
+          e.target.classList.add('selectedTemplate');
+  })
+
+
+
+
+
+});
+
+
+
+
