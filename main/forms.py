59aca1026ff5a9ecaf269bb42973dab8f5331172
tu-nguyen <valcrune@gmail.com>
2021-03-01 21:26:54 -0500

    prepping for form stylization

--- main/forms.py
+++ main/forms.py
@@ -18,13 +18,11 @@ FONT_CHOICES= [
 class StyleForm(forms.Form):
     template_style = forms.IntegerField(label="Template Style", widget = forms.HiddenInput(), initial=1, required=False)
     font_style= forms.CharField(label="Font Style", widget=forms.Select(choices=FONT_CHOICES), required=False)
-    font_size = forms.IntegerField(label="Font Size", min_value=1, max_value=3, initial=2, required=False)
+    font_size = forms.IntegerField(widget=forms.NumberInput(attrs={'type':'range', 'step': '1', 'min': '1', 'max': '3', 'id':'myRange'}),label="Font Size", min_value=1, max_value=3, initial=2, required=False)
     primary_color = forms.CharField(label="Primary Color", max_length=7, widget=forms.TextInput(attrs={'type': 'color'}))
     secondary_color = forms.CharField(label="Secondary Color", max_length=7, widget=forms.TextInput(attrs={'type': 'color'}))
 
 class DataForm(forms.Form):
-    # first_name_field = forms.CharField(label="First Name", max_length=50, required=True)
-    # last_name_field = forms.CharField(label="Last Name", max_length=50, required=True)
     first_name = forms.CharField(widget=forms.TextInput(attrs=dict(max_length=50, required=True)), label="First Name")
     last_name = forms.CharField(widget=forms.TextInput(attrs=dict(max_length=50, required=True)), label="Last Name")
     job_title = forms.CharField(label="Job Title", max_length=200, required=False)
